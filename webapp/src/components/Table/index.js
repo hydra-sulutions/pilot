6d7a39ca9461d8becc7283cf2ff7836a4053b638
Deivis <deiviswingert@hotmail.com>
2018-01-08 17:07:09 -0200

    addTable: add theme

--- webapp/src/components/Table/index.js
+++ webapp/src/components/Table/index.js
@@ -1,13 +1,24 @@
 import React from 'react'
-
+import {
+  shape,
+  string,
+} from 'prop-types'
 import { themr } from 'react-css-themr'
 
 const applyThemr = themr('UITable')
 
-const Table = () => (
-  <table>
+const Table = ({
+  theme,
+}) => (
+  <table className={theme.table}>
     <tr />
   </table>
 )
+
+Table.propTypes = {
+  theme: shape({
+    table: string,
+  }).isRequired,
+}
 
 export default applyThemr(Table)
