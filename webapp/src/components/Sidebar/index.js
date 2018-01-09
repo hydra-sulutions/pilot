7071b9d419042d053d5249738ebf2b545a217432
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2018-01-09 16:03:04 -0200

    sidebar: add things

--- webapp/src/components/Sidebar/index.js
+++ webapp/src/components/Sidebar/index.js
@@ -8,19 +8,73 @@ import cx from 'classnames'
 import PerfectScrollBar from 'react-perfect-scrollbar'
 import MdEventNote from 'react-icons/lib/md/event-note'
 import MdFreeBreakfast from 'react-icons/lib/md/free-breakfast'
+import IconArrowDown from 'react-icons/lib/md/keyboard-arrow-down'
 import SegmentedSwitch from '../SegmentedSwitch'
 
 const applyThrmr = themr('UISidebar')
 
 class Sidebar extends React.Component {
-  constructor () {
-    super()
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      selected: 'minha-conta',
+      collapsed: false,
+    }
 
     this.id = shortid.generate()
+
+    this.renderList = this.renderList.bind(this)
+    this.handleCollapsed = this.handleCollapsed.bind(this)
+  }
+
+  handleCollapsed () {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    })
+  }
+
+  renderList () {
+    const {
+      theme,
+      items,
+    } = this.props
+
+    const list = items.map((item) => {
+      const classes = cx(theme.item, {
+        [theme.itemSelected]: item.value === this.state.selected,
+      })
+
+      if (item.options) {
+        return (
+          <li className={classes}>
+            <p className={theme.text}>
+              <item.icon size={18} /> <span>{item.title} <IconArrowDown size={18} /></span>
+            </p>
+            {(item.value === this.state.selected && !this.state.collapsed) &&
+              <ul className={theme.options}>
+                {item.options.map(opt => (
+                  <li className={theme.option}><a className={theme.link}>{opt.title}</a></li>
+                ))}
+              </ul>
+            }
+          </li>
+        )
+      }
+
+      return (
+        <li className={classes}>
+          <a className={theme.link}><item.icon size={18} /> <span>{item.title}</span></a>
+        </li>
+      )
+    })
+
+    return list
   }
 
   render () {
     const {
+      items,
       theme,
       logo,
       title,
@@ -28,11 +82,18 @@ class Sidebar extends React.Component {
       onSwitchChange,
     } = this.props
 
+    const sidebarClasses = cx(theme.sidebar, {
+      [theme.collapsed]: this.state.collapsed,
+    })
+
     return (
-      <div className={theme.sidebar}>
+      <div className={sidebarClasses}>
         <header className={theme.header}>
           <img src={logo} alt={title} width={101} />
-          <button className={theme.menu}>
+          <button
+            className={theme.menu}
+            onClick={this.handleCollapsed}
+          >
             <MdMenu size={25} />
           </button>
         </header>
@@ -53,50 +114,7 @@ class Sidebar extends React.Component {
         <nav>
           <PerfectScrollBar className={theme.items}>
             <ul>
-              <li className={theme.item}>
-                <a className={theme.link}><MdEventNote size={18} /> Ola que tal</a>
-              </li>
-              <li className={cx(theme.item, theme.itemSelected)}>
-                <a className={theme.link}><MdEventNote size={18} /> Hello friend</a>
-              </li>
-              <li className={theme.item}>
-                <a className={theme.link}>Hi</a>
-              </li>
-              <li className={theme.item}>
-                <a className={theme.link}>Hi</a>
-              </li>
-              <li className={cx(theme.item, theme.itemSelected)}>
-                <p className={theme.text}>Hihihi ICONE-SETA-AQUI</p>
-                <ul className={theme.options}>
-                  <li className={theme.option}><a className={theme.link}>Hihihihihihihihihihiuhjiujhj</a></li>
-                  <li className={theme.option}><a className={theme.link}>Hi my friend</a></li>
-                </ul>
-              </li>
-              <li className={theme.item}>
-                <a className={theme.link}>Hi</a>
-              </li>
-              <li className={theme.item}>
-                <a className={theme.link}>Hi</a>
-                </li><li className={theme.item}>
-                <a className={theme.link}>Hi</a>
-                </li><li className={theme.item}>
-                <a className={theme.link}>Hi</a>
-                </li><li className={theme.item}>
-                <a className={theme.link}>Hi</a>
-                </li><li className={theme.item}>
-                <a className={theme.link}>Hi</a>
-                </li><li className={theme.item}>
-                <a className={theme.link}>Hi</a>
-                </li><li className={theme.item}>
-                <a className={theme.link}>Hi</a>
-              </li>
-              <li className={theme.item}>
-                <a className={theme.link}>Hi</a>
-                </li><li className={theme.item}>
-                <a className={theme.link}>Hi</a>
-                </li><li className={theme.item}>
-                <a className={theme.link}>Hi</a>
-              </li>
+              {this.renderList()}
             </ul>
           </PerfectScrollBar>
         </nav>
